b42ad35188979cdd60acc65f359880fc986f74ed
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2014-01-20 21:38:24 +0900

    packageの使用を開始。

--- .emacs.d/.cocoa-emacs.el
+++ .emacs.d/.cocoa-emacs.el
@@ -93,6 +93,11 @@
 
 ;(load "~/.emacs.d/setup-auto-complete")
 
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
 ;; Local variables:
 ;; coding: iso-2022-jp-unix
 ;; End:
