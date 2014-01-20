0157d729d19d4170f5bb851cbd6d7e29bfb98e00
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2014-01-20 21:35:34 +0900

    各種coding-systemの設定を追加。

--- .emacs.d/.cocoa-emacs.el
+++ .emacs.d/.cocoa-emacs.el
@@ -1,6 +1,11 @@
 (setq custom-file "~/.emacs.d/.cocoa-emacs-custom.el")
 
 (set-language-environment 'Japanese)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
 (prefer-coding-system 'utf-8)
 
 ;(load "term/bobcat")
