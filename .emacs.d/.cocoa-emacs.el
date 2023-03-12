4b2602a8ba7b1dd475d9d494aef6a86afdfbed90
IWATSUKI Hiroyuki <don@na.rim.or.jp>
2023-03-12 16:44:26 +0900

    カスタム設定の読み込みタイミングを変更。

--- .emacs.d/.cocoa-emacs.el
+++ .emacs.d/.cocoa-emacs.el
@@ -1,7 +1,3 @@
-(setq custom-file "~/.emacs.d/.cocoa-emacs-custom.el")
-(if (file-exists-p (expand-file-name custom-file))
-    (load (expand-file-name custom-file) t nil nil))
-
 (require 'init-auto-save-buffers)
 
 ;; splash screenを無効にする
@@ -9,6 +5,10 @@
 
 ;; backslash
 (define-key global-map [?\M-\] "\\") ;; Option+\でbackslash "\"
+
+(setq custom-file "~/.emacs.d/.cocoa-emacs-custom.el")
+(if (file-exists-p (expand-file-name custom-file))
+    (load (expand-file-name custom-file) t nil nil))
 
 ;; Local variables:
 ;; coding: iso-2022-jp-unix
