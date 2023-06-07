3c1f0e4749655f8cea3f6bec7833ce70d4c99fa5
IWATSUKI Hiroyuki <don@na.rim.or.jp>
2023-06-08 00:56:08 +0900

    更新。

--- .emacs.d/lisp/init-csharp-mode.el
+++ .emacs.d/lisp/init-csharp-mode.el
@@ -1,5 +1,9 @@
-(add-hook 'csharp-mode-hook
-	  (lambda ()
-	    (c-set-offset 'substatement-open 0)
-	    (c-set-offset 'case-label 0)
-	    ))
+(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
+(setq auto-mode-alist
+      (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
+
+;; (add-hook 'csharp-mode-hook
+;; 	  (lambda ()
+;; 	    (c-set-offset 'substatement-open 0)
+;; 	    (c-set-offset 'case-label 0)
+;; 	    ))
