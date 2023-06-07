72230d61f308d91092bab17470edf4a65486f856
IWATSUKI Hiroyuki <don@na.rim.or.jp>
2023-06-08 00:16:15 +0900

    Emacs 27,28の設定ファイルの内容を整えた。

--- .emacs.d/.emacs27.el
+++ .emacs.d/.emacs27.el
@@ -1,1 +1,18 @@
+(set-language-environment 'Japanese)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(require 'setup-elpa)
 (require 'init-auto-save-buffers)
+
+(setq custom-file "~/.emacs.d/.emacs27-custom.el")
+(if (file-exists-p (expand-file-name custom-file))
+    (load (expand-file-name custom-file) t nil nil))
+
+;; Local variables:
+;; coding: iso-2022-jp-unix
+;; End:
--- .emacs.d/.emacs28.el
+++ .emacs.d/.emacs28.el
@@ -1,4 +1,14 @@
+(set-language-environment 'Japanese)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(require 'setup-elpa)
 (require 'init-auto-save-buffers)
+(require 'custom-compile)
 
 (setq custom-file "~/.emacs.d/.emacs28-custom.el")
 (if (file-exists-p (expand-file-name custom-file))
