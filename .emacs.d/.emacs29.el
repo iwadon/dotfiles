91ba58d186af1e0ddd463c82a48ebdc227f4d94c
IWATSUKI Hiroyuki <don@na.rim.or.jp>
2023-12-22 16:46:16 +0900

    Create .emacs29.el

---
+++ .emacs.d/.emacs29.el
@@ -0,0 +1,22 @@
+(set-language-environment 'Japanese)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(require 'setup-elpa)
+(require 'init-auto-save-buffers)
+(require 'custom-compile)
+
+;; (if (eq system-type 'windows-nt)
+;;     (setq ls-lisp-UCA-like-collation nil))
+
+(setq custom-file "~/.emacs.d/.emacs28-custom.el")
+(if (file-exists-p (expand-file-name custom-file))
+    (load (expand-file-name custom-file) t nil nil))
+
+;; Local variables:
+;; coding: iso-2022-jp-unix
+;; End:
