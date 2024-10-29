5e898d175a4a556936c1903ff419e11932aa3de5
IWATSUKI Hiroyuki <don@na.rim.or.jp>
2024-10-29 13:31:42 +0900

    Update .emacs.d/.cocoa-emacs.el

--- .emacs.d/.cocoa-emacs.el
+++ .emacs.d/.cocoa-emacs.el
@@ -1,7 +1,15 @@
+(set-language-environment 'Japanese)
+(global-hl-line-mode t)
+(global-display-line-numbers-mode t)
+(tool-bar-mode 0)
+
 (require 'init-auto-save-buffers)
+(require 'setup-elpa)
+
+(load-theme 'dracula t)
 
 ;; splash screenを無効にする
-(setq inhibit-splash-screen t)
+;; (setq inhibit-splash-screen t)
 
 ;; backslash
 (define-key global-map [?\M-\] "\\") ;; Option+\でbackslash "\"
